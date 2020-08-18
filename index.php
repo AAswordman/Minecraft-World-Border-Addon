d2ea931e460c7132f4389f3c07da2c29b4d3dbf7
AAswordman <1002153674@qq.com>
2020-08-18 11:45:36 +0800

    m

---
+++ index.php
@@ -0,0 +1,133 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>奇怪的网页</title>
+
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+
+    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
+
+    <link href="css/styles.css" rel="stylesheet">
+  </head>
+  <body>
+    <div class="navbar-wrapper">
+      <div class="container">
+
+        <div class="navbar navbar-inverse navbar-static-top" role="navigation">
+          <div class="container">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">X</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="#">导航页</a>
+            </div>
+            <div class="navbar-collapse collapse">
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="#">主页</a></li>
+                <li><a href="#about">关于</a></li>
+                <li><a href="#contact">更多</a></li>
+
+              </ul>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="item active">
+          <img src="assets/main.png" alt="">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>导航页</h1>
+              <p>B.M.S蓝痕工作室 出品</p>
+              <p><a class="btn btn-lg btn-primary" href="http://bluemarkstudio.mysxl.cn/" role="button">点击进入蓝痕官网</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img src="assets/pvz.jpg" alt="">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>PVZ OL</h1>
+              <p>蓝痕科技PVZ OL制作中</p>
+              <p><a class="btn btn-lg btn-primary" href="http://pvzol.mysxl.cn/" role="button">点击申请内测</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img src="assets/wb.png" alt="">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>世界边境ADDON</h1>
+              <p>由aa剑侠制作的minecraft大型RPG addon</p>
+              <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+    </div>
+
+    <div class="container marketing">
+
+      
+      <div class="panel panel-primary">
+        <div class="col-lg-4">
+          <h2>MINECRAFT 闲聊/资源社区</h2>
+          <p>你可以在这个地方闲聊，或者下载别人的资源。</p>
+          <p><a class="btn btn-default" href="#" role="button">前往</a></p>
+        </div>
+        <div class="col-lg-4">
+          <h2>资源平台</h2>
+          <p>你可以在这里下载一些应用。</p>
+          <p><a class="btn btn-default" href="#" role="button">前往</a></p>
+        </div>
+        <div class="col-lg-4">
+          <h2>世界边境ADDON官网</h2>
+          <p>在这里下载最新的世界边境ADDON公开版本</p>
+          <p><a class="btn btn-default" href="#" role="button">前往</a></p>
+        </div>
+      </div>
+
+
+
+
+      <hr class="featurette-divider">
+	  
+     
+
+
+      <!-- FOOTER -->
+      <footer>
+        <p class="pull-right"><a href="#">Back to top</a></p>
+        <p>BMS蓝痕  ©2014-2020</p>
+      </footer>
+
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/docs.min.js"></script>
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
